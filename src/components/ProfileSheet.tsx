c402a5ef59beb505e7ea0497e304d7bffc61ba27
boualem-P <princeboualem24@gmail.com>
2026-03-12 14:01:08 +0100

    Update ProfileSheet.tsx

--- src/components/ProfileSheet.tsx
+++ src/components/ProfileSheet.tsx
@@ -371,7 +371,7 @@ const ProfileSheet = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 z-[400]"
+            className="fixed inset-0 z-[400] overflow-y-auto"
           >
             <PerfumePage
               perfume={selectedPerfume}

443f6b0b17744a88fb83a4c818ab0bb978769953
boualem-P <princeboualem24@gmail.com>
2026-03-12 09:15:00 +0100

    Update PyramidScreen.tsx

--- src/components/PyramidScreen.tsx
+++ src/components/PyramidScreen.tsx
@@ -383,7 +383,7 @@ const PyramidScreen = ({ onValidate, onMenu, setInternalBackHandler }: PyramidSc
                       <img
                         src={FAMILY_ICONS[f]!}
                         alt={f}
-                        className="w-5 h-5 object-contain"
+                        className="w-6 h-6 object-contain"
                         style={{
                           filter: "drop-shadow(0px 0px 2px rgba(212, 175, 55, 0.4))"
                         }}

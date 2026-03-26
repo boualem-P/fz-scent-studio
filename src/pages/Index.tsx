979af93689ebaa6e8c75871089e48baa660bd243
boualem-P <princeboualem24@gmail.com>
2026-03-26 10:24:38 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -165,15 +165,14 @@ const Index = () => {
 
 <main className="relative z-10 h-full w-full">
   <AnimatePresence mode="wait">
-    {!selectedPerfume && (
-      <motion.div 
-        key={screen} 
-        initial={{ opacity: 0, y: 10 }} 
-        animate={{ opacity: 1, y: 0 }} 
-        exit={{ opacity: 0 }} 
-        transition={{ duration: 0.15 }}
-        className="h-full w-full"
-      >
+    <motion.div 
+      key={screen} 
+      initial={{ opacity: 0, y: 10 }} 
+      animate={{ opacity: 1, y: 0 }} 
+      exit={{ opacity: 0, y: -10 }} 
+      transition={{ duration: 0.3 }}
+      className="h-full w-full"
+    >
               {screen === "landing" && (
                 <LandingScreen 
                   onSelectGender={handleGender} 
@@ -214,7 +213,6 @@ const Index = () => {
                 />
               )}
             </motion.div>
-          )}
         </AnimatePresence>
       </main>
 

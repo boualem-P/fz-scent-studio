44eb15ef4a204146908555e1efa1cea1497e20d0
boualem-P <princeboualem24@gmail.com>
2026-04-06 11:49:20 +0100

    Update ChatConseiller.tsx

--- src/components/ChatConseiller.tsx
+++ src/components/ChatConseiller.tsx
@@ -299,7 +299,7 @@ const ChatConseiller = () => {
         {open ? (
   <X size={22} className="text-black" />
 ) : (
-  <img src="/logo.png" alt="logo" className="w-12 h-12 object-contain" />
+  className="w-12 h-12 object-contain flex items-center justify-center mx-auto" />
 )}
 
       </motion.button>
